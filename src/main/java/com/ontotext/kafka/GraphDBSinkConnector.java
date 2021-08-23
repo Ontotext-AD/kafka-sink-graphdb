7dbe7795ffb692360fca092b57b74a79a85636e2
Tomas Kovachev <tomas.kovachev@ontotext.com>
2021-08-23 18:50:16 +0300

    GDB-6120 Added maven project + base Kafka API

---
+++ src/main/java/com/ontotext/kafka/GraphDBSinkConnector.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2017. Hans-Peter Grahsl (grahslhp@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ontotext.kafka;
+
+import org.apache.kafka.common.config.ConfigDef;
+import org.apache.kafka.connect.connector.Task;
+import org.apache.kafka.connect.sink.SinkConnector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.ontotext.kafka.util.PropertiesUtil;
+
+public class GraphDBSinkConnector extends SinkConnector {
+
+	private Map<String, String> properties;
+
+	@Override
+	public String version() {
+		return PropertiesUtil.getVersion();
+	}
+
+	@Override
+	public void start(Map<String, String> properties) {
+		this.properties = properties;
+	}
+
+	@Override
+	public Class<? extends Task> taskClass() {
+		return GraphDBSinkTask.class;
+	}
+
+	@Override
+	public List<Map<String, String>> taskConfigs(int maxTasks) {
+
+		List<Map<String, String>> taskConfigs = new ArrayList<>(maxTasks);
+
+		for (int i = 0; i < maxTasks; i++) {
+			taskConfigs.add(properties);
+		}
+
+		return taskConfigs;
+
+	}
+
+	@Override
+	public void stop() {}
+
+	@Override
+	public ConfigDef config() {
+		return GraphDBSinkConnectorConfig.conf();
+	}
+}
---
+++ src/test/java/com/ontotext/kafka/GraphDBSinkConnectorTest.java
@@ -0,0 +1,14 @@
+package com.ontotext.kafka;
+
+import org.junit.jupiter.api.Test;
+
+class GraphDBSinkConnectorTest {
+
+	@Test
+	void start() {
+	}
+
+	@Test
+	void stop() {
+	}
+}
