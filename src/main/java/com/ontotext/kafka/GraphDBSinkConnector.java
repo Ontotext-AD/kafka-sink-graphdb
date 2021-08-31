39d098600e1a5a0605ce79e1d0c4fb0ad627fe51
Tomas Kovachev <tomas.kovachev@ontotext.com>
2021-08-31 10:10:18 +0300

    GDB-6120 Cleaned up classes

--- src/main/java/com/ontotext/kafka/GraphDBSinkConnector.java
+++ src/main/java/com/ontotext/kafka/GraphDBSinkConnector.java
@@ -37,7 +37,6 @@ import com.ontotext.kafka.util.PropertiesUtil;
 public class GraphDBSinkConnector extends SinkConnector {
 
 	private Map<String, String> properties;
-	private GraphDBSinkConfig.AuthenticationType authType;
 
 	@Override
 	public String version() {
