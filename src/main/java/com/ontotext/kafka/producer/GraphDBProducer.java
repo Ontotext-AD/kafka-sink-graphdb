3aa0e4dc2722970757435dcbed8b3389e6cba04d
vasilnv <vasil_vasilev1999@abv.bg>
2021-09-07 16:58:06 +0300

    GDB-6139 Fix shecll script for automating the process of starting the producer and make it read from more than one file

--- src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
+++ src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
@@ -3,51 +3,75 @@ package com.ontotext.kafka.producer;
 import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class GraphDBProducer {
-    private static String kafkaBrokerEndpoint = null;
+    private static String configFile = null;
     private static String kafkaTopic = null;
-    private static String file = null;
+    private static List<String> allFiles = null;
 
-    private Producer<String, String> getProducer() {
-        Properties properties = new Properties();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokerEndpoint);
-        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "GraphDBProducer");
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+    private Producer<String, String> getProducer(Properties props) {
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-        return new KafkaProducer<String, String>(properties);
+        return new KafkaProducer<String, String>(props);
     }
 
     public static void main(String[] args) {
-        if (args != null) {
-            kafkaBrokerEndpoint = args[0];
-            kafkaTopic = args[1];
-            file = args[2];
+        if (args.length < 3) {
+            System.out.println("Please provide command line arguments: configPath topic filesToReadFrom...");
+            System.exit(1);
         }
 
+        configFile = args[0];
+        kafkaTopic = args[1];
+        allFiles = Arrays.asList(args).subList(2, args.length);
         GraphDBProducer producer = new GraphDBProducer();
         producer.publish();
     }
 
     private void publish() {
-        final Producer<String, String> producer = getProducer();
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line = reader.readLine();
-            while(line != null) {
-                final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(kafkaTopic, UUID.randomUUID().toString(), line);
-                producer.send(producerRecord).get();
-                line = reader.readLine();
-            }
-        } catch (IOException | InterruptedException | ExecutionException e) {
+        Properties props = null;
+        try {
+            props = loadConfig(configFile);
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+        final Producer<String, String> producer = getProducer(props);
+
+        for (String file : allFiles) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line = reader.readLine();
+                while(line != null) {
+                    final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(kafkaTopic, null, line);
+                    producer.send(producerRecord).get();
+                    line = reader.readLine();
+                }
+            } catch (IOException | InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static Properties loadConfig(final String configFile) throws IOException {
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+        final Properties cfg = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            cfg.load(inputStream);
+        }
+        cfg.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        cfg.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        return cfg;
     }
 }
--- startProducer.sh
+++ startProducer.sh
@@ -1,3 +1,6 @@
-cd ~
-java -classpath IdeaProjects/kafka-sink-graphdb/target/classes:.m2/repository/org/slf4j/slf4j-api/1.7.30/slf4j-api-1.7.30.jar:.m2/repository/org/apache/kafka/kafka-clients/2.8.0/kafka-clients-2.8.0.jar:.m2/repository/org/slf4j/slf4j-simple/1.7.22/slf4j-simple-1.7.22.jar com.ontotext.kafka.producer.GraphDBProducer $@
-cd -
+#cd ~
+#java -classpath IdeaProjects/kafka-sink-graphdb/target/classes:.m2/repository/org/slf4j/slf4j-api/1.7.30/slf4j-api-1.7.30.jar:.m2/repository/org/apache/kafka/kafka-clients/2.4.0/kafka-clients-2.4.0.jar:.m2/repository/org/slf4j/slf4j-simple/1.7.22/slf4j-simple-1.7.22.jar com.ontotext.kafka.producer.GraphDBProducer $@
+#cd -
+#java -cp kafka-clients.jar:slf4j-api.jar:slf4j-simple.jar:kafka-sink-graphdb.jar com/ontotext/kafka/producer/RunProducer $@
+# $@ = /home/vasil/kafka_2.13-2.8.0/config/graphdb-producer.properties graphdb testKafka.ttl
+java -cp kafka-clients.jar:slf4j-api.jar:slf4j-simple.jar:kafka-sink-graphdb.jar com/ontotext/kafka/producer/GraphDBProducer $@
