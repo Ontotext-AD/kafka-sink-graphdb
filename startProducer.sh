2de30ff1553c94ac3eb49bea856b60512825aa3f
vasilnv <vasil_vasilev1999@abv.bg>
2021-09-08 17:41:07 +0300

    GDB-6139 Update script to start the producer from any directory

--- startProducer.sh
+++ startProducer.sh
@@ -1,6 +1,9 @@
-#cd ~
-#java -classpath IdeaProjects/kafka-sink-graphdb/target/classes:.m2/repository/org/slf4j/slf4j-api/1.7.30/slf4j-api-1.7.30.jar:.m2/repository/org/apache/kafka/kafka-clients/2.4.0/kafka-clients-2.4.0.jar:.m2/repository/org/slf4j/slf4j-simple/1.7.22/slf4j-simple-1.7.22.jar com.ontotext.kafka.producer.GraphDBProducer $@
-#cd -
-java -cp kafka-clients.jar:slf4j-api.jar:slf4j-simple.jar:kafka-sink-graphdb.jar com/ontotext/kafka/producer/RunProducer $@
-# $@ = /home/vasil/kafka_2.13-2.8.0/config/graphdb-producer.properties graphdb testKafka.ttl
-#java -cp kafka-clients.jar:slf4j-api.jar:slf4j-simple.jar:kafka-sink-graphdb.jar com/ontotext/kafka/producer/GraphDBProducer /home/vasil/kafka_2.13-2.8.0/config/graphdb-producer.properties graphdb testKafka.ttl testKafka1.ttl
+if [ $# -lt 4 ]
+  then
+    echo "./startProducer.sh <plugins dir> <producer config properties file path> <kafka topic> <files to add data from> ..."
+    exit 1
+fi
+
+cd "$1/kafka-sink-graphdb/"
+shift
+java -cp kafka-clients.jar:slf4j-api.jar:slf4j-simple.jar:kafka-sink-graphdb.jar com.ontotext.kafka.producer.RunProducer $@
