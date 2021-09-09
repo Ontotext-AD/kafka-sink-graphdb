c63fa10f4177ec4c214640620de725bc285fe777
vasilnv <vasil_vasilev1999@abv.bg>
2021-09-09 15:40:31 +0300

    GDB-6139 Update producer to send the whole data from file as a message

--- src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
+++ src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
@@ -10,31 +10,40 @@ public class GraphDBProducer<K,V> extends KafkaProducer<K,V>{
     private final List<String> allFiles;
     private final String kafkaTopic;
     private final Properties properties;
+    private final String keySeparator;
 
     public GraphDBProducer(List<String> files, String kafkaTopic, Properties properties) {
         super(properties);
         this.properties = properties;
         this.allFiles = files;
         this.kafkaTopic = kafkaTopic;
+        this.keySeparator = properties.getProperty("graphdb.key.separator");
     }
 
     public void publish() {
         for (String file : this.allFiles) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 String line = reader.readLine();
+                String key = null;
+                if (this.keySeparator != null && line.contains(this.keySeparator)) {
+                    key = line.split(this.keySeparator)[0];
+                    line = line.split(this.keySeparator)[1];
+                }
+                StringBuilder builder = new StringBuilder();
                 while(line != null) {
-                    //TODO how to pass key for replace and updates?
-                    final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(this.kafkaTopic, null, line);
-                    super.send((ProducerRecord<K, V>) producerRecord, new Callback() {
-                        @Override
-                        public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                            if(e != null) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).get();
+                    builder.append(line).append("\n");
                     line = reader.readLine();
                 }
+                final ProducerRecord<String, String> producerRecord = new ProducerRecord<>(this.kafkaTopic, key, builder.toString());
+                super.send((ProducerRecord<K, V>) producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                        if(e != null) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).get();
+
             } catch (IOException | InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
