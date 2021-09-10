d31adc48df3b852ad0ebe6d96d2f57af2700780f
dobri <dobri.dimitrov@ontotext.com>
2021-09-10 17:42:40 +0300

    optimizing setQuery

--- src/main/java/com/ontotext/kafka/service/UpdateRecordsProcessor.java
+++ src/main/java/com/ontotext/kafka/service/UpdateRecordsProcessor.java
@@ -1,23 +1,17 @@
 package com.ontotext.kafka.service;
 
-import com.ontotext.kafka.util.PropertiesUtil;
 import com.ontotext.kafka.util.ValueUtil;
 import org.apache.kafka.common.errors.TimeoutException;
-import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.sink.SinkRecord;
-import org.eclipse.rdf4j.query.UpdateExecutionException;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
-import org.eclipse.rdf4j.repository.RepositoryException;
 import org.eclipse.rdf4j.rio.RDFFormat;
-import org.eclipse.rdf4j.rio.RDFParseException;
-import org.eclipse.rdf4j.rio.UnsupportedRDFormatException;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.ontotext.kafka.util.PropertiesUtil.TEMPLATE_ID;
@@ -25,9 +19,11 @@ import static com.ontotext.kafka.util.PropertiesUtil.getProperty;
 import static com.ontotext.kafka.util.ValueUtil.convertValueToString;
 
 public class UpdateRecordsProcessor extends SinkRecordsProcessor {
+	//todo remove @merge
+	long NUMBER_OF_CONNECTION_RETRIES = 5L;
+	long DEFERRED_TIME_BETWEEN_RETRIES = 100L;
 
-	private final long LINGER_BETWEEN_RETRIES = 100L;
-	private final long TOTAL_RETRY_TIME = 30_000L;
+	private final StringBuilder sb = new StringBuilder();
 
 	protected UpdateRecordsProcessor(Queue<Collection<SinkRecord>> sinkRecords, AtomicBoolean shouldRun,
 									 Repository repository, RDFFormat format, int batchSize, long timeoutCommitMs) {
@@ -35,102 +31,83 @@ public class UpdateRecordsProcessor extends SinkRecordsProcessor {
 	}
 
 	@Override
-	protected void flushRecordUpdates() {
+	public void flushRecordUpdates() {
 		try {
-			if (!recordsBatch.isEmpty()) {//or 'while'?
-				Queue<SinkRecord> records = new LinkedBlockingQueue<>(recordsBatch);
-				int flushed = records.size();
-
-				flush(records);
-				//removeFlushedRecords(flushed);
-				//clearFailed();
+			if (!recordsBatch.isEmpty()) {
+				flush();
+				recordsBatch.clear();
 			}
 		} catch (TimeoutException e) {
-			//todo handle timeout?
+			//todo LogErrorHandler.logError(e.getMessage());
 			throw new RuntimeException(e);
 		}
 	}
 
-	private void flush(Queue<SinkRecord> records) throws TimeoutException {
-		long startFlush = System.currentTimeMillis();
+	private void flush() throws TimeoutException {
+		long retryCount = 1L;
+		Queue<SinkRecord> failedRecords = new ArrayDeque<>(recordsBatch.size());
 
 		do {
+			removeFailed(failedRecords);
 			long startAttempt = System.currentTimeMillis();
 			try (RepositoryConnection connection = repository.getConnection()) {
 				connection.begin();
-				for (SinkRecord record : records) {
-					updateRecord(record, connection);
+				for (SinkRecord record : recordsBatch) {
+					updateRecord(record, connection, failedRecords);
 				}
 				connection.commit();
 				return;
 
-			} catch (IOException | RepositoryException e) {
-				//retry sending all records in the batch until successful or timeout
-				while (LINGER_BETWEEN_RETRIES < System.currentTimeMillis() - startAttempt) {
+			} catch (Exception e) {
+				//todo LogErrorHandler.logWarning("Exception while flushing records " + e.getMessage());
+				//retry sending all valid records in the batch until successful or timeout
+				while (DEFERRED_TIME_BETWEEN_RETRIES > System.currentTimeMillis() - startAttempt) {
 					//do nothing
 				}
-				if (TOTAL_RETRY_TIME > System.currentTimeMillis() - startFlush) {
+				if (NUMBER_OF_CONNECTION_RETRIES <= retryCount) {
 					throw new TimeoutException("Flushing run out of time due to: " + e.getMessage());
 				}
+				//todo	LogErrorHandler.logInfo("Retrying connection...");
+				retryCount++;
 			}
 		} while (true);
 	}
 
-	private void updateRecord(SinkRecord record, RepositoryConnection connection) throws IOException {
+	private void removeFailed(Queue<SinkRecord> failedRecords) {
+		while (!failedRecords.isEmpty()) {
+			recordsBatch.remove(failedRecords.poll()); //remove malformed record when retrying the connection
+		}
+	}
+
+	private void updateRecord(SinkRecord record, RepositoryConnection connection,
+							  Queue<SinkRecord> failedRecords) throws IOException {
 		try {
 			String query = getQuery(record);
 			connection.prepareUpdate(query)
 					.execute();
+			connection.add(ValueUtil.convertRDFData(record.value()), format);
 
-			connection.add(ValueUtil
-					.convertRDFData(record.value()), format);
-
-		} catch (UpdateExecutionException | NullPointerException | RDFParseException |
-				UnsupportedRDFormatException | DataException | RepositoryException e) {
+		} catch (Exception e) {
+			if (e instanceof IOException)
+				throw new IOException("IOException during updateRecord");
 			// Catch records that caused exceptions we can't recover from by retrying the connection
-			//catchMalformedRecords(record, e);
+			//todo errorHandler.handleFailingRecord(record, e);
+			failedRecords.offer(record);
 		}
 	}
 
-	private String getQuery(SinkRecord record) {
-		String templateId = Objects.requireNonNull(getProperty(TEMPLATE_ID), "Cannot update with empty templateId");
+	private String getQuery(SinkRecord record) throws NullPointerException {
+		String templateId = Objects
+				.requireNonNull(getProperty(TEMPLATE_ID), "Cannot update with empty templateId");
 		String templateBinding = convertValueToString(record.key());
 
-		return "PREFIX onto: <http://www.ontotext.com/>\n" +
-				"insert data {\n" +
-				"    onto:smart-update onto:sparql-template <" + templateId + ">;\n" +
-				"               onto:template-binding-id <" + templateBinding + "> .\n" +
-				"}\n";
+		sb.setLength(0);
+		return sb.append("PREFIX onto: <http://www.ontotext.com/>\n")
+				.append("insert data {\n")
+				.append("    onto:smart-update onto:sparql-template <").append(templateId).append(">;\n")
+				.append("               onto:template-binding-id <").append(templateBinding).append("> .\n")
+				.append("}\n")
+				.toString();
 	}
-
-/*
-
-	private void updateRecord(SinkRecord record, RepositoryConnection connection) throws IOException {
-		try {
-			String query = getQuery(record);
-			connection.prepareUpdate(query)
-					.execute();
-
-		} catch (UpdateExecutionException | NullPointerException | RDFParseException |
-				UnsupportedRDFormatException | DataException | RepositoryException e) {
-			// Catch records that caused exceptions we can't recover from by retrying the connection
-			//catchMalformedRecords(record, e);
-		}
-	}
-
-	private String getQuery(SinkRecord record) {
-		String templateId = Objects.requireNonNull(getProperty(TEMPLATE_ID), "Cannot update with empty templateId");
-
-		String templateBinding = convertValueToString(record.key());
-		String recordRDFData = convertValueToString(record.value());
-
-		return "PREFIX onto: <http://www.ontotext.com/>\n" +
-				"insert data {\n" +
-				"    onto:smart-update onto:sparql-template <" + templateId + ">;\n" +
-				"               onto:template-binding-id <" + templateBinding + "> .\n" +
-				recordRDFData +
-				"}\n";
-	}
-*/
 
 }
