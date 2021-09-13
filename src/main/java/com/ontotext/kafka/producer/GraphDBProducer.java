f75e78e8cdcd0cbf11f38cf118f9e7f926aa21df
vasilnv <vasil.vasilev1999@abv.bg>
2021-09-14 02:29:13 +0300

    GDB-6139 Fix Producer to convert data from files correctly

--- src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
+++ src/main/java/com/ontotext/kafka/producer/GraphDBProducer.java
@@ -1,15 +1,15 @@
 package com.ontotext.kafka.producer;
 
-import com.ontotext.load.GraphdbRDFLoader;
-import com.ontotext.trree.GraphDBParserConfig;
 import org.apache.kafka.clients.producer.*;
-import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.query.GraphQueryResult;
+import org.eclipse.rdf4j.query.QueryResults;
 import org.eclipse.rdf4j.rio.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
@@ -18,7 +18,6 @@ public class GraphDBProducer<K,V> extends KafkaProducer<K,V>{
     private final List<String> allFiles;
     private final String kafkaTopic;
     private final Properties properties;
-    private final String keySeparator;
     private Long updatesPauses = null;
     private static final Logger LOG = LoggerFactory.getLogger(GraphDBProducer.class);
 
@@ -28,7 +27,6 @@ public class GraphDBProducer<K,V> extends KafkaProducer<K,V>{
         this.properties = properties;
         this.allFiles = files;
         this.kafkaTopic = kafkaTopic;
-        this.keySeparator = properties.getProperty("graphdb.key.separator");
         if (properties.getProperty("graphdb.updates.time.pauses") != null) {
             this.updatesPauses = Long.parseLong(properties.getProperty("graphdb.updates.time.pauses"));
         }
@@ -61,69 +59,76 @@ public class GraphDBProducer<K,V> extends KafkaProducer<K,V>{
         for (int i = 0; i < allFiles.size(); i += 2) {
             String dataFile = allFiles.get(i);
             String keysFile = allFiles.get(i + 1);
-            File tempFile = null;
             try {
-                tempFile = File.createTempFile("prefix-", "-suffix");
-
                 InputStream dataStream = new FileInputStream(dataFile);
                 RDFFormat inputFormat = Rio.getParserFormatForFileName(dataFile).orElse(RDFFormat.TURTLE);
 
-                RDFParser rdfParser = Rio.createParser(inputFormat);
-                RDFWriter rdfWriter = Rio.createWriter(RDFFormat.NQUADS,
-                        new FileOutputStream(tempFile.getName()));
-
-                rdfParser.setRDFHandler(rdfWriter);
-                rdfParser.parse(dataStream, dataStream.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-//            try {
-//                InputStream dataStream = new FileInputStream(dataFile);
-//                RDFFormat inputFormat = Rio.getParserFormatForFileName(dataFile).orElse(RDFFormat.TURTLE);
-//
-//                GraphdbRDFLoader loader = new GraphdbRDFLoader(GraphDBParserConfig.newInstance(), SimpleValueFactory.getInstance());
-//                try {
-//                    tempFile = File.createTempFile("prefix-", "-suffix");
-//                    RDFWriter rdfWriter = Rio.createWriter(RDFFormat.NQUADS,
-//                            new FileOutputStream(tempFile.getName()));
-//                    loader.load(new FileInputStream(dataFile), null, inputFormat, rdfWriter);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            } catch (FileNotFoundException e) {
-//                e.printStackTrace();
-//            }
-//
-
-            try (BufferedReader dataReader = new BufferedReader(new FileReader(tempFile.getName()));
-                BufferedReader keysReader = new BufferedReader(new FileReader(keysFile))) {
-                String line = dataReader.readLine();
-                String keyName = keysReader.readLine().split("=")[1];
-                String keyStatements = keysReader.readLine().split("=")[1];
-                int numberOfStatementsPerKey = Integer.parseInt(keyStatements);
-                StringBuilder builder = new StringBuilder();
-                int countStatementsInCurrentMessage = 0;
-                while(line != null) {
-                    builder.append(line).append("\n");
-                    countStatementsInCurrentMessage++;
-                    if (countStatementsInCurrentMessage == numberOfStatementsPerKey) {
-                        sendMessage(keyName, builder.toString().getBytes());
-                        if ( (keyName = keysReader.readLine()) != null) {
-                            keyStatements = keysReader.readLine().split("=")[1];
-                            numberOfStatementsPerKey = Integer.parseInt(keyStatements);
-                            builder = new StringBuilder();
-                            countStatementsInCurrentMessage = 0;
+                try (GraphQueryResult res = QueryResults.parseGraphBackground(dataStream,dataFile, inputFormat);
+                     BufferedReader keysReader = new BufferedReader(new FileReader(keysFile))) {
+                    String keyLine = keysReader.readLine();
+                    String keyName = keyLine.split("=")[1];
+                    String keyStatements = keysReader.readLine().split("=")[1];
+                    int numberOfStatementsPerKey = Integer.parseInt(keyStatements);
+                    int countStatementsInCurrentMessage = 0;
+                    StringBuilder builder = new StringBuilder();
+                    while (res.hasNext()) {
+                        Statement st = res.next();
+                        convertStatementToNQString(builder, st);
+                        countStatementsInCurrentMessage++;
+                        if (countStatementsInCurrentMessage == numberOfStatementsPerKey) {
+                            sendMessage(keyName, builder.toString().getBytes());
+                            if ( (keyLine = keysReader.readLine()) != null) {
+                                keyName = keyLine.split("=")[1];
+                                keyStatements = keysReader.readLine().split("=")[1];
+                                numberOfStatementsPerKey = Integer.parseInt(keyStatements);
+                                builder = new StringBuilder();
+                                countStatementsInCurrentMessage = 0;
+                            } else {
+                                break;
+                            }
                         }
                     }
-                    line = dataReader.readLine();
+                    if (keyName != null) {
+                        sendMessage(keyName, builder.toString().getBytes());
+                    }
                 }
-                sendMessage(keyName, builder.toString().getBytes());
-
             } catch (IOException e) {
                 LOG.error(e.getMessage());
             }
         }
+    }
+
+    private void convertStatementToNQString(StringBuilder builder, Statement st) {
+        String subj = "<" + st.getSubject().toString() + ">";
+        String pred = "<" + st.getPredicate().toString() + ">";
+        String obj = "<" + st.getObject().toString() + ">";
+        String context = "";
+
+        if (st.getSubject().isBNode()) {
+            subj = "_:" + ((BNode) st.getSubject()).getID();
+        }
+
+        if (st.getPredicate().isBNode()) {
+            pred = "_:" + ((BNode) st.getPredicate()).getID();
+        }
+
+        if (st.getObject().isBNode()) {
+            obj = "_:" + ((BNode) st.getObject()).getID();
+        } else if (st.getObject().isLiteral()) {
+            obj = st.getObject().toString();
+        }
+
+        if (st.getContext() != null) {
+            if (st.getContext().isBNode()) {
+                context = "_:" + ((BNode) st.getContext()).getID();
+            } else {
+                context = "<" + st.getContext().toString() + ">";
+            }
+        }
+
+        builder.append(subj).append(" ")
+                .append(pred).append(" ")
+                .append(obj).append(" ").append(context)
+                .append(".").append("\n");
     }
 }
