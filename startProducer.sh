7cecdc99daeb5b2ab36742654f5a0f3bac14b37f
vasilnv <vasil_vasilev1999@abv.bg>
2021-09-14 12:12:41 +0300

    GDB-6139 Fix shell script

--- startProducer.sh
+++ startProducer.sh
@@ -1,11 +1,9 @@
 if [ $# -lt 4 ]
   then
-    echo "./startProducer.sh <project dir> <producer config properties file path> <kafka topic> <file with keys> <files to add data from> ..."
+    echo "./startProducer.sh <producer config properties file path> <kafka topic> <file with keys> <files to add data from> ..."
     exit 1
 fi
 
-cd "$1/target/"
-shift
+cd "target/"
 
 java -cp graphdb-kafka-plugin.jar com.ontotext.kafka.producer.RunProducer $@
-rm graphdb-kafka-plugin.jar
