7877d813deb65c5e898e318137c5eb439defcb93
nikolay.kolev <nikolay.kolev@ontotext.com>
2024-12-17 16:01:54 +0200

    xx

--- src/main/java/com/ontotext/kafka/producer/RunProducer.java
+++ src/main/java/com/ontotext/kafka/producer/RunProducer.java
@@ -10,12 +10,12 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class RunProducer {
-    private static final Logger LOG = LoggerFactory.getLogger(RunProducer.class);
+//    private static final Logger LOG = LoggerFactory.getLogger(RunProducer.class);
 
 	// FIXME: what is this class?! Is this the entry point? Either document or remove it.
     public static void main(String[] args) {
         if (args.length < 3) {
-            LOG.error("Please provide command line arguments: configPath topic filesToReadFrom...");
+//            LOG.error("Please provide command line arguments: configPath topic filesToReadFrom...");
             throw new RuntimeException("Wrong number of command line arguments");
         }
 
@@ -27,7 +27,7 @@ public class RunProducer {
         try {
             props = config.loadConfig(configFile);
         } catch (IOException e) {
-            LOG.error(e.getMessage(), e);
+//            LOG.error(e.getMessage(), e);
         }
 		// FIXME: this is auto closable and has to be used in try with resources
         GraphDBProducer<String, String> producer = new GraphDBProducer<>(allFiles, kafkaTopic, props);
