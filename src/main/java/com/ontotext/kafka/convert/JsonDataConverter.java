3ca97480624ad7b6ed9aee8df46e6d5c41043503
Ivan Konstantinov <ivan.konstantinov@ontotext.com>
2025-02-17 13:10:13 +0200

    Remove main method

--- src/main/java/com/ontotext/kafka/convert/JsonDataConverter.java
+++ src/main/java/com/ontotext/kafka/convert/JsonDataConverter.java
@@ -4,14 +4,9 @@ import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ontotext.kafka.graphdb.template.ValueParser;
-import com.ontotext.kafka.util.ValueUtil;
-import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.rio.RDFFormat;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public final class JsonDataConverter extends RdfFormatConverter {
@@ -51,22 +46,4 @@ public final class JsonDataConverter extends RdfFormatConverter {
 		}
 		return null;
 	}
-
-	public static void main(String[] args) throws IOException {
-		byte[] data = Files.readAllBytes(Paths.get(args[0]));
-		JsonDataConverter converter = new JsonDataConverter(ValueUtil.getRDFFormat(args[1]));
-		Map<String, Object> map = converter.convert(data);
-		ObjectMapper objectMapper1 = new ObjectMapper();
-		System.out.println(objectMapper1.writerWithDefaultPrettyPrinter().writeValueAsString(map));
-		for (String key : map.keySet()) {
-			Object obj = map.get(key);
-			Value value = ValueParser.getValue(obj);
-			if (value == null) {
-				continue;
-			}
-			System.out.printf("%s : %s\n", key, value);
-		}
-	}
-
-
 }
