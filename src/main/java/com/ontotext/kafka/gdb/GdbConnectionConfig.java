032a065bc68cd882cbcb90da68dd570d5ce51ad6
Ivan Konstantinov <ivan.konstantinov@ontotext.com>
2025-06-25 22:14:02 +0300

    Certificate-based authentication
    Implementation of:
    - mTLS authentication (standard)
    - Header-based certificate auth (see
      https://graphdb.ontotext.com/documentation/11.0/access-control.html#using-x-509-certificate-authentication-with-http-headers)

--- src/main/java/com/ontotext/kafka/gdb/GdbConnectionConfig.java
+++ src/main/java/com/ontotext/kafka/gdb/GdbConnectionConfig.java
@@ -1,12 +1,15 @@
 package com.ontotext.kafka.gdb;
 
 import com.ontotext.kafka.GraphDBSinkConfig;
+import com.ontotext.kafka.gdb.auth.AuthHeaderConfig;
+import com.ontotext.kafka.gdb.auth.MtlsConfig;
+import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.common.config.Config;
 import org.apache.kafka.common.config.ConfigValue;
 import org.apache.kafka.common.config.types.Password;
 
+import java.util.Base64;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -21,11 +24,19 @@ public class GdbConnectionConfig {
 	private final String username;
 	private final Password password;
 	private final boolean hostnameVerificationEnabled;
+	private final MtlsConfig mtlsConfig;
+	private final AuthHeaderConfig authHeaderConfig;
+
 
 	public GdbConnectionConfig(GraphDBSinkConfig config) {
 		this(config.getServerUrl(), config.getRepositoryId(), config.getTlsThumbprint(), config.isHostnameVerificationEnabled(), config.getAuthType(),
 			config.getAuthBasicUser(),
-			config.getAuthBasicPassword());
+			config.getAuthBasicPassword(),
+			config.getAuthMTlsClientCertificate(),
+			config.getAuthMTlsClientCertificateKey(),
+			config.getAuthMTlsClientCertificateKeyPassword(),
+			config.getAuthCertificateHeaderString(),
+			config.getAuthCertificateHeaderName());
 	}
 
 	public GdbConnectionConfig(Config config) {
@@ -35,21 +46,46 @@ public class GdbConnectionConfig {
 		this.repositoryId = getValue(valueMap.get(REPOSITORY), true);
 		this.tlsThumbprint = getValue(valueMap.get(TLS_THUMBPRINT), false);
 		this.authType = AuthenticationType.valueOf(getValue(valueMap.get(AUTH_TYPE), true).toUpperCase());
-		this.username = getValue(valueMap.get(AUTH_BASIC_USER), this.authType != GraphDBSinkConfig.AuthenticationType.NONE);
-		this.password = getValue(valueMap.get(AUTH_BASIC_PASS), Password.class, this.authType != GraphDBSinkConfig.AuthenticationType.NONE);
+		this.username = getValue(valueMap.get(AUTH_BASIC_USER), this.authType == AuthenticationType.BASIC);
+		this.password = getValue(valueMap.get(AUTH_BASIC_PASS), Password.class, this.authType == AuthenticationType.BASIC);
 		this.hostnameVerificationEnabled = getValue(valueMap.get(HOSTNAME_VERIFICATION), Boolean.class, false);
+
+		Base64.Decoder b64Decoder = Base64.getDecoder();
+		String mTlsClientCertificate = decode(getValue(valueMap.get(MTLS_CERTIFICATE_STRING), String.class, this.authType == AuthenticationType.MTLS));
+		String mTlsClientCertificateKey = decode(getValue(valueMap.get(MTLS_CERTIFICATE_KEY_STRING), String.class, this.authType == AuthenticationType.MTLS));
+		Password mTlsClientCertificateKeyPassword = getValue(valueMap.get(MTLS_CERTIFICATE_KEY_PASSWORD), Password.class, false);
+		String authHeaderCertificate = decode(getValue(valueMap.get(AUTH_HEADER_CERTIFICATE_STRING), String.class, this.authType == AuthenticationType.X509_HEADER));
+		String authHeaderName = getValue(valueMap.get(AUTH_HEADER_NAME), String.class, this.authType == AuthenticationType.X509_HEADER);
+
+		this.mtlsConfig = new MtlsConfig(mTlsClientCertificate, mTlsClientCertificateKey, mTlsClientCertificateKeyPassword);
+		this.authHeaderConfig = new AuthHeaderConfig(authHeaderCertificate, authHeaderName);
 	}
 
 	public GdbConnectionConfig(String serverUrl, String repositoryId, String tlsThumbprint, boolean hostnameVerificationEnabled,
-							   GraphDBSinkConfig.AuthenticationType authType, String authBasicUser,
-							   Password authBasicPassword) {
-		this.serverUrl = getValue(serverUrl, true);
-		this.repositoryId = getValue(repositoryId, true);
-		this.tlsThumbprint = getValue(tlsThumbprint, false);
-		this.authType = getValue(authType, true);
-		this.username = getValue(authBasicUser, this.authType != GraphDBSinkConfig.AuthenticationType.NONE);
-		this.password = getValue(authBasicPassword, this.authType != GraphDBSinkConfig.AuthenticationType.NONE);
-		this.hostnameVerificationEnabled = getValue(hostnameVerificationEnabled, false);
+							   AuthenticationType authType, String authBasicUser,
+							   Password authBasicPassword, String mTlsClientCertificate, String mTlsClientCertificateKey, Password mTlsClientCertificateKeyPassword, String authHeaderCertificate,
+							   String authHeaderName) {
+		this.serverUrl = getValue(SERVER_URL, serverUrl, true);
+		this.repositoryId = getValue(REPOSITORY, repositoryId, true);
+		this.tlsThumbprint = getValue(TLS_THUMBPRINT, tlsThumbprint, false);
+		this.authType = getValue(AUTH_TYPE, authType, true);
+		this.username = getValue(AUTH_BASIC_USER, authBasicUser, this.authType == AuthenticationType.MTLS);
+		this.password = getValue(AUTH_BASIC_PASS, authBasicPassword, this.authType == AuthenticationType.MTLS);
+		this.hostnameVerificationEnabled = getValue(HOSTNAME_VERIFICATION, hostnameVerificationEnabled, false);
+
+		Base64.Decoder b64Decoder = Base64.getDecoder();
+		// Validation of values provided
+		mTlsClientCertificate = decode(getValue(MTLS_CERTIFICATE_STRING, mTlsClientCertificate, this.authType == AuthenticationType.MTLS));
+		mTlsClientCertificateKey = decode(getValue(MTLS_CERTIFICATE_KEY_STRING, mTlsClientCertificateKey, this.authType == AuthenticationType.MTLS));
+		authHeaderCertificate = decode(getValue(AUTH_HEADER_CERTIFICATE_STRING, authHeaderCertificate, this.authType == AuthenticationType.X509_HEADER));
+		authHeaderName = getValue(AUTH_HEADER_NAME, authHeaderName, this.authType == AuthenticationType.X509_HEADER);
+		this.mtlsConfig = new MtlsConfig(mTlsClientCertificate, mTlsClientCertificateKey, mTlsClientCertificateKeyPassword);
+		this.authHeaderConfig = new AuthHeaderConfig(authHeaderCertificate, authHeaderName);
+
+	}
+
+	private String decode(String value) {
+		return StringUtils.isNotEmpty(value) ? new String(Base64.getDecoder().decode(value)) : value;
 	}
 
 	public String getServerUrl() {
@@ -77,6 +113,14 @@ public class GdbConnectionConfig {
 	}
 
 
+	public MtlsConfig getMtlsConfig() {
+		return mtlsConfig;
+	}
+
+	public AuthHeaderConfig getAuthHeaderConfig() {
+		return authHeaderConfig;
+	}
+
 	public boolean isHostnameVerificationEnabled() {
 		return hostnameVerificationEnabled;
 	}
@@ -88,7 +132,7 @@ public class GdbConnectionConfig {
 
 	private <T> T getValue(ConfigValue value, Class<T> type, boolean failOnNull) {
 		try {
-			return type.cast(getValue(value.value(), failOnNull));
+			return type.cast(getValue(value.name(), value.value(), failOnNull));
 		} catch (NullPointerException e) {
 			throw new GdbConnectionConfigException(value.name(), null, "Must provide value");
 		} catch (ClassCastException e) {
@@ -97,10 +141,11 @@ public class GdbConnectionConfig {
 
 	}
 
-	private <T> T getValue(T val, boolean failOnNull) {
-		if (failOnNull) {
-			return Objects.requireNonNull(val);
+	private <T> T getValue(String configName, T val, boolean failOnNull) {
+		if (failOnNull && val == null) {
+			throw new GdbConnectionConfigException(configName, null, "Must provide value");
 		}
 		return val;
 	}
+
 }
