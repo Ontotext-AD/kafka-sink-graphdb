1130923efe7b96b3c95477868e837e6434d95dfe
Ivan Konstantinov <ivan.konstantinov@ontotext.com>
2025-07-03 21:23:45 +0300

    Fix version check to only allow GDB versions 10.8+, 11+ and 9.11+

--- src/test/java/com/ontotext/kafka/ConfigurationValidationTest.java
+++ src/test/java/com/ontotext/kafka/ConfigurationValidationTest.java
@@ -39,7 +39,7 @@ public class ConfigurationValidationTest {
 	@Timeout(5)
 	void testWithAllCorrect(MockServerClient mockedGraphDBClient) {
 		mockedGraphDBClient = new MockServerClient("localhost", 12345);
-		setupMockClientResponses(mockedGraphDBClient, "basic", "9.10.0");
+		setupMockClientResponses(mockedGraphDBClient, "basic", "9.11.0");
 		configs =
 			new HashMap<>() {
 				{
@@ -69,7 +69,7 @@ public class ConfigurationValidationTest {
 	@Timeout(5)
 	void testWithDefaults(MockServerClient mockedGraphDBClient) {
 		mockedGraphDBClient = new MockServerClient("localhost", 12345);
-		setupMockClientResponses(mockedGraphDBClient, "none", "9.10.0");
+		setupMockClientResponses(mockedGraphDBClient, "none", "9.11.0");
 		configs =
 			new HashMap<>() {
 				{
@@ -88,7 +88,7 @@ public class ConfigurationValidationTest {
 	@Timeout(5)
 	void testWithWrongCredentials(MockServerClient mockedGraphDBClient) {
 		mockedGraphDBClient = new MockServerClient("localhost", 12345);
-		setupMockClientResponses(mockedGraphDBClient, "basic", "9.10.0");
+		setupMockClientResponses(mockedGraphDBClient, "basic", "9.11.0");
 		configs =
 			new HashMap<>() {
 				{
